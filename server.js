ffb998a4522482dd25eb96e21e51ae18dc65c1fb
picnic20 <ohmygod112255@gmail.com>
2026-07-18 22:51:34 +0900

    mongoDB collection test2

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ if (getApps().length === 0) {
 // 🍃 [MongoDB 연결 설정]
 // =================================================================
 const MONGODB_URI = process.env.VITE_MONGODB_URI ;
-mongoose.connect(MONGODB_URI,{dbName: 'sample_mflix', dbcollection'users'})
+mongoose.connect(MONGODB_URI,{dbName: 'sample_mflix', dbcollection : 'users'})
   .then(() => console.log("🍃 MongoDB 'sample_mflix' 데이터베이스 연결 성공!"))
   .catch(err => console.error("❌ MongoDB 연결 실패:", err));
 
